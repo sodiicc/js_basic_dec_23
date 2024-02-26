6e0318e22c722b48ff688444856558b80ccd12e2
Vasyl Tsiva <perec649@gmail.com>
2024-02-26 21:11:28 +0200

    added lesson 16

---
+++ lesson_16/script.js
@@ -0,0 +1,75 @@
+// const a = 'A symbol'
+// const b = 'B symbol'
+
+// const myObj = {
+//   name: 'Bob',
+//   houses: null
+// }
+
+// const housesCount = prompt('Enter your houses count pls')
+
+// if (housesCount) myObj.houses = ['house 1']
+// // console.log('myAge', myAge)
+// // let yearOfBithday
+
+// try {
+//   console.log('houses count', myObj.houses.length)
+//   // yearOfBithday = new Date().getFullYear() - myAge
+//   // throw new Error('Good Morning')
+// } catch (error) {
+//   console.error('error', error)
+// } finally {
+//   console.log('This if Finally block')
+// }
+
+// // console.log('yearOfBithday', yearOfBithday)
+// console.log('A', a)
+// console.log('B', b)
+
+
+
+// fetch('https://jsonplaceholder.typicode.com/todos')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+
+
+function logA() {console.log('A')}
+function logB() {console.log('B')}
+function logC() {console.log('C')}
+
+const myBtn = document.querySelector('button')
+
+myBtn.addEventListener('click', onBtnClick)
+
+
+function onBtnClick() {
+  console.log('Button Clicked')
+}
+
+logA()
+setTimeout(logB, 0)
+
+// for(let i = 0; i < 5_000_000_000; i++) {
+
+// }
+
+logC()
+
+const black = document.querySelector('.black')
+const black2 = document.querySelector('.black2')
+
+// let left = 0
+// let left2 = 0
+
+// setInterval(() => {
+//   left += 1
+//   black.style.left = `${left}px`
+// }, 10)
+
+// setInterval(() => {
+//   left2 += 1
+//   black2.style.left = `${left2}px`
+// }, 5)
+
+
+
